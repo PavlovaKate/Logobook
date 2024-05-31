c123464040fccb19962c0ba361fd311a1f984f0f
andrey6446 <andrey6446@yandex.ru>
2024-05-31 14:06:19 +0300

    feat:navbar-top

---
+++ client/src/App/provider/AppRoutes.tsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+// import Main from '../../pages/Main/Main';
+// import Places from '../../pages/Place/component/Places';
+// import Error from '../../pages/ErrorPage/Error';
+// import PlacePage from '../../pages/Place/component/PlacePage';
+// import RegistrationPage from '../../pages/Auth/RegistrationPage';
+// import AuthorizationPage from '../../pages/Auth/AuthorizationPage';
+// import RoutesPage from '../../pages/Route/components/RoutesPage';
+
+function AppRoutes(): JSX.Element {
+  return (
+    <div className="AppRoutes">
+      <Routes>
+        {/* <Route path="/" element={<Main />} />
+        <Route path="/places" element={<Places />} />
+        <Route path="/places/:id" element={<PlacePage />} />
+        <Route path="/routes" element={<RoutesPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/authorization" element={<AuthorizationPage />} />
+        <Route path="*" element={<Error />} /> */}
+      </Routes>
+    </div>
+  );
+}
+
+export default AppRoutes;
