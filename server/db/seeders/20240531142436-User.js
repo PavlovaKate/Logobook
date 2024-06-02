2f0d5d0824f46d8a5adfff4146b0b1648ccfbf11
andrey6446 <andrey6446@yandex.ru>
2024-06-02 13:05:48 +0300

    fix:app

--- server/db/seeders/20240531142436-User.js
+++ server/db/seeders/20240531142436-User.js
@@ -9,6 +9,7 @@ module.exports = {
         {
           name: 'Andrey',
           email: 'andrey6446@yandex.ru',
+          isAdmin: false,
           password: '$2b$10$TucrXRPlbe.C8uq8RNLWL.6WPLRFgSJXSG8JvxWpfLg.YZYUrMwMa',
           image:
             'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExN3l5eXZ6dTJpMWtndm8yZGplOG9kbzR2NHFxZmQ1ZjV1OG4zZ2tjZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/aQwvKKi4Lv3t63nZl9/giphy.gif',
@@ -24,6 +25,7 @@ module.exports = {
         {
           name: 'Nastya',
           email: 'azarovan13@yandex.ru',
+          isAdmin: false,
           password: '$2b$10$TucrXRPlbe.C8uq8RNLWL.6WPLRFgSJXSG8JvxWpfLg.YZYUrMwMa',
           image:
             'https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg',
@@ -31,12 +33,14 @@ module.exports = {
         {
           name: 'Misha',
           email: 'andrey6446@ya.ru',
+          isAdmin: false,
           password: '$2b$10$TucrXRPlbe.C8uq8RNLWL.6WPLRFgSJXSG8JvxWpfLg.YZYUrMwMa',
           image: 'https://pics.craiyon.com/2023-07-07/579e523aca6a48ec8c7729dfed888f87.webp',
         },
         {
           name: 'Katya',
           email: '123@123',
+          isAdmin: false,
           password: '$2b$10$TucrXRPlbe.C8uq8RNLWL.6WPLRFgSJXSG8JvxWpfLg.YZYUrMwMa',
           image: 'https://pics.craiyon.com/2023-07-07/579e523aca6a48ec8c7729dfed888f87.webp',
         },
