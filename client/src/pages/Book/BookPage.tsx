d763fb49dc9bfe735c5e3bef49ee01ad29e6766c
PavlovaKate <kv.pavlova@mail.ru>
2024-06-03 11:47:08 +0300

    feat: style

--- client/src/pages/Book/BookPage.tsx
+++ client/src/pages/Book/BookPage.tsx
@@ -1,75 +1,87 @@
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import type { RootState } from '../../App/store/store';
 import Error from '../ErrorPage/Error';
 import NavBar from '../Navbar/NavBar';
-// import type { RootState } from '../../../App/store/store';
+import type { Book } from '../Main/type/type';
+import './Book.css';
 
 function BookPage(): JSX.Element {
-  // const places = useSelector((store: RootState) => store.places.places);
-  const books = [
-    {
-      id: 1,
-      title: 'Code. Носители',
-      description:
-        'ПРОДОЛЖЕНИЕ БЕСТСЕЛЛЕРА «THE ONE. ЕДИНСТВЕННЫЙ»Триллер в духе «Черного зеркала» от лауреата премии International Thriller Writers Award 2021Лучший фантастический триллер-2021 по версии Wall Street Journal«Если хочешь сохранить секрет, надо скрывать его и от себя». – Джордж Оруэлл, «1984»В XXI веке информация – бог. Но как ее защитить? Любое хранилище можно взломать, кроме...Несколько обычных людей выбраны правительством для участия в уникальной программе защиты данных. После новейших биоинженерных операций они становятся ходячими тайниками, Носителями. Отныне их мозг содержит сверхсекретные сведения, зашифрованные в генетическом коде. Взамен они получили шанс решить все свои проблемы и начать жизнь с чистого листа.Вместе Носители знают все грязные тайны, всю правду, скрывающуюся за каждой государственной ложью, за каждой теорией заговора. Но можно ли им доверять? Ведь у них тоже есть секреты, и они сделают все, чтобы их защитить...––«Роман прямо напрашивается: ""NETFLIX, экранизируй меня!""» – My Chestnut Reading Tree«С этой книгой я, словно кролик, выхваченный из тьмы фарами, зачарованно смотрел на источник света – и был безжалостно сбит тем, что оказалось за ним». – Books from Dusk till Dawn«Завораживающе и в то же время невероятно пугающе». – Totally Booked«Увлекательный и крайне правдоподобный триллер Маррса поднимает интересные вопросы о нашем будущем, где наука станет играть первую скрипку». – Booklist«Чрезвычайно мощная комбинация умного сюжета, проблем технофобии, конспирологического триллера и шокирующих личных историй». – SFX Magazine«Маррс поистине блестящ в лихих поворотах сюжета и адреналиновой гонке повествования». – Питер Джеймс«Шок на каждой следующей странице». – Wall Street Journal',
-      pageCount: 388,
-      imageSmall:
-        'http://books.google.com/books/content?id=u0ZkEAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
-      image:
-        'http://books.google.com/books/content?id=u0ZkEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
-      saleability: false,
-      quantity: 650,
-      amount: 498.75,
-      authorId: 315,
-      categoryId: 1,
-      publisherId: 23,
-    },
-    {
-      id: 2,
-      title: 'Вся кремлевская рать: Краткая история современной России',
-      description:
-        'Эта книга рассказывает об истории России на всем протяжении правления Владимира Путина, с 2000 по 2015 год. В основу книги легли документы, открытые источники и десятки уникальных личных интервью, которые автор взял у действующих лиц из ближайшего окружения Владимира Путина. Собранные воедино, факты, события, интриги и мнения героев составляют полную картину жизни Кремля, из которой впервые становится понятна логика метаморфозы Владимира Путина: как и почему из либерального прозападного президента начала 2000-х он превратился в авторитарного правителя и одного из самых ярых противников Запада.',
-      pageCount: 453,
-      imageSmall:
-        'http://books.google.com/books/content?id=OisACwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
-      image:
-        'http://books.google.com/books/content?id=OisACwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
-      saleability: true,
-      quantity: 525,
-      amount: 549,
-      authorId: 435,
-      categoryId: 2,
-      publisherId: 14,
-    },
-  ];
   const navigate = useNavigate();
+  const books = useSelector((store: RootState) => store.book.books);
 
   const { id } = useParams();
 
   if (id) {
     const book = books.find((bk) => bk.id === +id);
 
+    const booksByAuthor = books.filter((bk) => bk.author === book?.author && bk.id !== book.id);
+    const booksByCategory = books.filter(
+      (bk) => bk.category === book?.category && bk.id !== book.id,
+    );
+
     if (book) {
       return (
         <>
           <NavBar />
-          <div className="BookPage">
-            <img src={book?.image} alt={book?.title} />
-            <h3>{book?.title}</h3>
-            <h4>автор</h4>
-            <p>рейтинг</p>
-            <p>издательство</p>
-            <p>Количество страниц: {book?.pageCount}</p>
-            <p>{book.amount}</p>
-            <button type="button">добавить в корзину</button>
-            <p>Описание</p>
-            <p>{book?.description}</p>
+          <div className="BookPage container">
+            <div className="BookBlok">
+              <div className="BookCover">
+                <img src={book?.image} alt={book?.title} />
+                {/* <div>
+                  {book.TagLines.map((tag) => (
+                    <p>{tag.Tag.tagname}</p>
+                  ))}
+                </div> */}
+              </div>
+              <div className="BookInfo">
+                <h2>{book?.title}</h2>
+                <h3>{book?.author}</h3>
+                <p>{book?.RateLine.Rate.rateAvg}</p>
+                <div>
+                  <p>Издательство</p>
+                  <p>{book?.publisher}</p>
+                  <p>Количество страниц</p>
+                  <p>{book?.pageCount}</p>
+                  <p>Наличие</p>{' '}
+                  <div>
+                    {book.ShopLines.map((sh) => (
+                      <p>
+                        {sh.number} шт. - {sh.Shop.adress}
+                      </p>
+                    ))}
+                  </div>
+                </div>
+                <p className="Price">{book?.amount} ₽</p>
+                <button type="button">добавить в корзину</button>
+              </div>
+            </div>
+            <div>
+              <p>Описание</p>
+              <p>{book?.description}</p>
+            </div>
+
+            <p>{book.Reviews.map((el) => el.review)}</p>
+            <p>
+              {book.Reviews.map((el) =>
+                el.Images.map((img) => <img src={img.image} alt="фото отзыв" />),
+              )}
+            </p>
+
             <p>Другие книги от автора</p>
-            <div>книги этого автора</div>
+            <div>
+              {booksByAuthor.map((el: Book) => (
+                <p>{el.title}</p>
+              ))}
+            </div>
             <p>С этой книгой покупают</p>
-            <div>книги этого жанра</div>
+            <div>
+              {booksByCategory.map((el: Book, ind: number) => {
+                if (ind < 8) return <p>{el.title}</p>;
+              })}
+            </div>
             <button type="button" onClick={() => navigate(-1)}>
               назад
             </button>
--- client/src/pages/Main/type/type.ts
+++ client/src/pages/Main/type/type.ts
@@ -9,8 +9,61 @@ export type Book = {
   quantity: number;
   amount: number;
   authorId: number;
+  author: string;
+  category: string;
+  publisher: string;
   categoryId: number;
   publisherId: number;
+  Favourites: Fav[];
+  RateLine: RateLine;
+  Reviews: Review[];
+  ShopLines: ShopLine[];
+  TagLines: TagLine[];
 };
 
 export type BookId = Book['id'];
+
+export type Fav = {
+  id: number;
+  usedId: number;
+  bookId: number;
+};
+
+export type RateLine = {
+  Rate: { rateAvg: number };
+};
+
+export type Review = {
+  Images: { image: string }[];
+  bookId: number;
+  id: number;
+  review: string;
+  userId: number;
+};
+
+export type Shop = {
+  adress: string;
+  id: number;
+  latitude: number;
+  longitude: number;
+  workTime: string;
+};
+export type ShopLine = {
+  id: number;
+  Shop: Shop;
+  bookId: number;
+  number: number;
+  shopId: Shop['id'];
+};
+
+export type TagLine = {
+  id: number;
+  Tag: Tag;
+  bookId: number;
+  tagId: number;
+};
+
+export type Tag = {
+  id: number;
+  tagname: string;
+};
